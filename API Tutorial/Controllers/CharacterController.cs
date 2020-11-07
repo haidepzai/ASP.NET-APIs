22b523b44b743c380ad4f1431529085287a2e071
Son Hai Vu <Zerato@hotmail.de>
2020-11-07 13:54:43 +0100

    Initial commit

---
+++ API Tutorial/Controllers/CharacterController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Tutorial.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Tutorial.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character { Id = 1, Name = "Sam"}
+        };
+
+        [HttpGet("GetAll")]
+        public IActionResult Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(characters[id]);
+        }
+    }
+}
