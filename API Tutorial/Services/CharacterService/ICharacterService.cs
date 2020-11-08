320fea8f1af8ba238cf7c66ddf4be31f8e60527b
Son Hai Vu <Zerato@hotmail.de>
2020-11-08 11:58:11 +0100

    Added asynchronous calls

--- API Tutorial/Services/CharacterService/ICharacterService.cs
+++ API Tutorial/Services/CharacterService/ICharacterService.cs
@@ -8,9 +8,9 @@ namespace API_Tutorial.Services.CharacterService
 {
     public interface ICharacterService
     {
-        List<Character> GetAllCharacters();
-        Character GetCharacterById(int id);
-        List<Character> AddCharacter(Character newCharacter);
+        Task<List<Character>> GetAllCharacters();
+        Task<Character> GetCharacterById(int id);
+        Task<List<Character>> AddCharacter(Character newCharacter);
 
     }
 }
